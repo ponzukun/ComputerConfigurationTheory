d539a4d00313d5a46a89236f35c0a06e632ba51c
ponzukun <p1o1n1z1u@gmail.com>
2023-04-10 14:06:37 +0900

    プロジェクト ファイルを追加します。

---
+++ ComputerConfigurationTheory/ComputerConfigurationTheory.cpp
@@ -0,0 +1,10 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    std::cout << "氏名：土井 裕輔\n"
+        << "所属する専門分野：データサイエンス\n"
+        << "現在住んでいる都市：京都市\n"
+        << "知っているコンピュータ言語：SQL, Python, JavaScript, TypeScript, Ruby, Java\n";
+}
