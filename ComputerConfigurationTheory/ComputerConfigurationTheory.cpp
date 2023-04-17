9ace365d7d280bcbcd5615113f7466253b783d48
ponzukun <p1o1n1z1u@gmail.com>
2023-04-17 14:19:00 +0900

    第4回 プログラム17

--- ComputerConfigurationTheory/ComputerConfigurationTheory.cpp
+++ ComputerConfigurationTheory/ComputerConfigurationTheory.cpp
@@ -1,30 +1,28 @@
-﻿/*整数加算の実行（キーボード入力）*/
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 
 int main(void)
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int quotient = 0;
-	int modResult = 0;
-
-	printf("aの値を入力してください\n");
-	scanf_s("%d", &a);
-	printf("aの値の確認=%d\n", a);
-
-	printf("bの値を入力してください\n");
-	scanf_s("%d", &b);
-	printf("bの値の確認=%d\n", b);
-	
-	c = int(pow(a, 3) + pow(b, 4));
-	quotient = c / 8;
-	modResult = c % 8;
-	
-	printf("商 %d\n", quotient);
-	printf("余り %d\n", modResult);
+	int A[] = { 3, 5, 1, 4, 9, 2, 6, 10, 8, 7 };
+	int i, n;
+	int max, min, max_index, min_index;
+	max = A[0];
+	min = A[0];
+	max_index = 0;
+	min_index = 0;
+	n = 10;
+	printf("入力値");
+	for (i = 0; i <= n - 1; i++) printf("A[%d]=%d", i, A[i]);
+	printf("\n");
+	for (i = 1; i <= n - 1; i++)
+	{
+		if (A[i] > max) {
+			max = A[i]; max_index = i;
+		}
+		if (A[i] < min) {
+			min = A[i]; min_index = i;
+		}
+	}
+	printf("max=%d max_index=%d min=%d min_index=%d\n", max, max_index, min, min_index);
 }
-
