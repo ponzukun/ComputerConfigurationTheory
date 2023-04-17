599b201f960d96e28b01ce098cccf3ad268f0183
ponzukun <p1o1n1z1u@gmail.com>
2023-04-17 15:00:01 +0900

    第4回レポート課題 (1)

--- ComputerConfigurationTheory/ComputerConfigurationTheory.cpp
+++ ComputerConfigurationTheory/ComputerConfigurationTheory.cpp
@@ -1,28 +1,40 @@
-﻿#include <stdio.h>
+﻿#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
+#include <vector>
 
+using namespace std;
 
 int main(void)
 {
-	int A[] = { 3, 5, 1, 4, 9, 2, 6, 10, 8, 7 };
+	int A[] = { 3,5,1,10,9,2,6,10,8,1 };
 	int i, n;
-	int max, min, max_index, min_index;
+	int max, min;
 	max = A[0];
 	min = A[0];
-	max_index = 0;
-	min_index = 0;
-	n = 10;
-	printf("入力値");
-	for (i = 0; i <= n - 1; i++) printf("A[%d]=%d", i, A[i]);
-	printf("\n");
-	for (i = 1; i <= n - 1; i++)
-	{
-		if (A[i] > max) {
-			max = A[i]; max_index = i;
-		}
-		if (A[i] < min) {
-			min = A[i]; min_index = i;
-		}
+	n = sizeof A / sizeof A[0];
+	vector<int> max_indexes;
+	vector<int> min_indexes;
+
+	cout << "入力値";
+	for (i = 0; i <= n - 1; i++) cout << "A[" << i << "] = " << A[i];
+	cout << endl;
+
+	for (i = 1; i <= n - 1; i++) {
+		if (A[i] > max) max = A[i];
+		if (A[i] < min) min = A[i];
 	}
-	printf("max=%d max_index=%d min=%d min_index=%d\n", max, max_index, min, min_index);
+
+	for (i = 1; i <= n - 1; i++) {
+		if (A[i] == max) max_indexes.push_back(i);
+		if (A[i] == min) min_indexes.push_back(i);
+	}
+
+	cout << "最大値=" << max << ", 最小値=" << min;
+	cout << endl;
+	cout << "最大値インデックス ";
+	for (i = 0; i < max_indexes.size(); i++) cout << max_indexes[i] << ", ";
+	cout << endl;
+	cout << "最小値インデックス ";
+	for (i = 0; i < min_indexes.size(); i++) cout << min_indexes[i] << ", ";
 }
